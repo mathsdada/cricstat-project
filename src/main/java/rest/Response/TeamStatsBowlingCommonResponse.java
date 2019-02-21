ebc5b82055955670b2c460f9e625321ec0b769c1
vgangadh <gangadhar.nitw@gmail.com>
2019-02-21 21:16:00 +0530

    Team Stats => Bowling => Missed Changes

--- src/main/java/rest/Response/TeamStatsBowlingCommonResponse.java
+++ src/main/java/rest/Response/TeamStatsBowlingCommonResponse.java
@@ -5,32 +5,30 @@ import java.math.BigDecimal;
 public class TeamStatsBowlingCommonResponse {
     private final String bowler;
     private final int innings;
+    private final BigDecimal overs;
+    private final int maidens;
     private final int runs;
-    private final int balls;
+    private final int wickets;
     private final BigDecimal average;
+    private final BigDecimal economy;
     private final BigDecimal strikeRate;
-    private final int highScore;
-    private final int fours;
-    private final int sixes;
-    private final int ducks;
-    private final int fifties;
-    private final int hundreds;
-    private final int notOuts;
+    private final int aboveFourWickets;
+    private final int aboveFiveWickets;
 
-    public TeamStatsBowlingCommonResponse(String bowler, int innings, int runs, int balls, BigDecimal average, BigDecimal strikeRate, int highScore, int fours, int sixes, int ducks, int fifties, int hundreds, int notOuts) {
+    public TeamStatsBowlingCommonResponse(String bowler, int innings, BigDecimal overs, int maidens, int runs,
+                                          int wickets, BigDecimal average, BigDecimal economy, BigDecimal strikeRate,
+                                          int aboveFourWickets, int aboveFiveWickets) {
         this.bowler = bowler;
         this.innings = innings;
+        this.overs = overs;
+        this.maidens = maidens;
         this.runs = runs;
-        this.balls = balls;
+        this.wickets = wickets;
         this.average = average;
+        this.economy = economy;
         this.strikeRate = strikeRate;
-        this.highScore = highScore;
-        this.fours = fours;
-        this.sixes = sixes;
-        this.ducks = ducks;
-        this.fifties = fifties;
-        this.hundreds = hundreds;
-        this.notOuts = notOuts;
+        this.aboveFourWickets = aboveFourWickets;
+        this.aboveFiveWickets = aboveFiveWickets;
     }
 
     public String getBowler() {
@@ -41,47 +39,39 @@ public class TeamStatsBowlingCommonResponse {
         return innings;
     }
 
+    public BigDecimal getOvers() {
+        return overs;
+    }
+
+    public int getMaidens() {
+        return maidens;
+    }
+
     public int getRuns() {
         return runs;
     }
 
-    public int getBalls() {
-        return balls;
+    public int getWickets() {
+        return wickets;
     }
 
     public BigDecimal getAverage() {
         return average;
     }
 
+    public BigDecimal getEconomy() {
+        return economy;
+    }
+
     public BigDecimal getStrikeRate() {
         return strikeRate;
     }
 
-    public int getHighScore() {
-        return highScore;
+    public int getAboveFourWickets() {
+        return aboveFourWickets;
     }
 
-    public int getFours() {
-        return fours;
-    }
-
-    public int getSixes() {
-        return sixes;
-    }
-
-    public int getDucks() {
-        return ducks;
-    }
-
-    public int getFifties() {
-        return fifties;
-    }
-
-    public int getHundreds() {
-        return hundreds;
-    }
-
-    public int getNotOuts() {
-        return notOuts;
+    public int getAboveFiveWickets() {
+        return aboveFiveWickets;
     }
 }
